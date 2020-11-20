130d7f5475ecd679d77950cf052cd1ff242c1a4e
PranjalAgni <pranjal.agnihotri6@gmail.com>
2020-11-20 22:54:27 +0530

    :zap: develop(app.ts): minor change

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { SlateServer } from "./services/slate";
 const app = express();
 const server = http.createServer(app);
 
+console.log("This is just a line in develop branch");
 new SlateServer(server);
 
 export default server;
